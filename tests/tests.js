d38098e1cb43d849f8302f9449441fc5f0a09c0c
Dan Reeves <heydanreeves@gmail.com>
2015-01-10 01:49:00 +0000

    one superficial test

---
+++ tests/tests.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = require('assert'),
+    please = require('../dist/please.js')();
+
+
+describe('please', function () {
+    it('should be an object', function () {
+        assert.equal('object', typeof please);
+    });
+});
