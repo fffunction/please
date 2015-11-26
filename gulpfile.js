27c6e46e74cd15a454ceb59f5d0c5c640591df7f
Dan Reeves <heydanreeves@gmail.com>
2015-11-26 17:59:36 +0000

    switch to ava and eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 'use strict';
 var gulp     = require('gulp'),
-    mocha    = require('gulp-mocha'),
     header   = require('gulp-header'),
     jshint   = require('gulp-jshint'),
     rename   = require('gulp-rename'),
-    uglify   = require('gulp-uglify'),
-    istanbul = require('gulp-istanbul');
+    eslint   = require('gulp-eslint'),
+    ava      = require('gulp-ava'),
+    uglify   = require('gulp-uglify');
 
 var pkg = require('./package.json'),
     banner = ['/**',
@@ -17,10 +17,27 @@ var pkg = require('./package.json'),
               ' */',
               ''].join('\n');
 
-gulp.task('jshint', function () {
-    return gulp.src('src/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+gulp.task('lint', function () {
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['src/*.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', function () {
+    return gulp.src('test/*.js')
+        // gulp-ava needs filepaths so you can't have any plugins before it
+        .pipe(ava());
 });
 
 gulp.task('build', function () {
@@ -36,18 +53,4 @@ gulp.task('build', function () {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('mocha', function (cb) {
-    gulp.src('src/*.js')
-        .pipe(istanbul())
-        .pipe(istanbul.hookRequire())
-        .on('finish', function () {
-            gulp.src(['tests/*.js'])
-                .pipe(mocha())
-                .pipe(istanbul.writeReports())
-                .on('end', cb);
-        });
-});
-
-
-gulp.task('tests', ['jshint', 'mocha']);
-gulp.task('default', ['jshint', 'mocha', 'build']);
+gulp.task('default', ['lint', 'test', 'build']);
---
+++ test/please.js
@@ -0,0 +1,6 @@
+import test from 'ava';
+
+test(t => {
+    t.pass();
+    t.end();
+});
